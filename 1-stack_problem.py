8a8ef9e474be4d911bc8c1278295737bc7879965
danjsan <113567967+danjsan@users.noreply.github.com>
2024-04-16 20:40:53 -0600

    Update 1-stack_problem.py

--- 1-stack_problem.py
+++ 1-stack_problem.py
@@ -5,6 +5,6 @@
 
 regular_list = ['orange','banana','grape','onion','peach','apple','kiwi']
 
-#Hint: use the pop operation to get to onion and store it in one variable to later it print it as shown below
+#Hint: use the pop operation to get to the impostor and store it in one variable to later it print it as shown below
 
 print(f'The impostor was {your_undefined_variable}')
